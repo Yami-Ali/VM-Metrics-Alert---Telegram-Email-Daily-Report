03ca05895ba8404ba99bd891f0d73ff8646c53ae
Yami-Ali <alimohammedmahdi512@gmail.com>
2026-06-01 22:51:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo bash /opt/vm-metrics/vm_metrics_reporter.sh --uninstall 2>/dev/null
-curl -fsSL "https://raw.githubusercontent.com/Yami-Ali/VM-Metrics-Alert---Telegram-Email-Daily-Report/main/vm_metrics_reporter%20.sh" -o vm_metrics_reporter.sh
+curl -fsSL "https://raw.githubusercontent.com/Yami-Ali/VM-Metrics-Alert---Telegram-Email-Daily-Report/main/vm_metrics_reporter.sh" -o vm_metrics_reporter.sh
 sed -i 's/\r//' vm_metrics_reporter.sh
 sudo bash vm_metrics_reporter.sh --install
